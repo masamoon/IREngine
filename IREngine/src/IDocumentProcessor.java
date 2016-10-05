020703c9e2cee6c46a7d0b6675351253ea390cc8
andre.lopes@ua.pt <andre.lopes@ua.pt>
2016-10-05 22:01:27 +0100

    still basic structure

---
+++ IREngine/src/IDocumentProcessor.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Andre on 05/10/2016.
+ */
+public interface IDocumentProcessor {
+
+    public void process(Document document);
+
+    public void indexFile(Document document);
+}
